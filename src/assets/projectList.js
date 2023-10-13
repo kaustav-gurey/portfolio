81d7c1261ad6b6512cac51f34f6a2dd02e3c54ef
kaustav-gurey <kaustav.gurey0607@gmail.com>
2023-10-13 13:17:30 +0530

    Fixed an issue with image paths

--- src/assets/projectList.js
+++ src/assets/projectList.js
@@ -1,21 +1,21 @@
 export default [
     {
         id: 1, 
-        image: "../src/assets/images/project-tic-tac-toe.png", 
+        image: "/src/assets/images/project-tic-tac-toe.png", 
         title: "Tic-tac-toe", 
         githubLink:"https://github.com/kaustav-gurey/tic-tac-toe", 
         liveLink: "https://tic-tac-toe-kg.netlify.app/"
     },
     {
         id: 2, 
-        image: "../src/assets/images/project-payment-portal.png", 
+        image: "/src/assets/images/project-payment-portal.png", 
         title: "Payment Website", 
         githubLink:"https://github.com/kaustav-gurey/TheSparksFoundation", 
         liveLink: "https://splendid-rabanadas-10a6f6.netlify.app/"
     },
     {
         id: 3, 
-        image: "../src/assets/images/project-tic-tac-toe.png", 
+        image: "/src/assets/images/project-tic-tac-toe.png", 
         title: "Tic-tac-toe", 
         githubLink:"https://github.com/kaustav-gurey/tic-tac-toe", 
         liveLink: "https://tic-tac-toe-kg.netlify.app/"
