f9cba964f4d5b62134213ed7d9c7515fcce3167a
kaustav-gurey <kaustav.gurey0607@gmail.com>
2023-10-13 14:14:30 +0530

    Fixed bugs

--- src/assets/projectList.js
+++ src/assets/projectList.js
@@ -1,23 +1,24 @@
+
 export default [
     {
         id: 1, 
-        image: "/src/assets/images/project-tic-tac-toe.png", 
+        image: "project-tic-tac-toe.png", 
         title: "Tic-tac-toe", 
         githubLink:"https://github.com/kaustav-gurey/tic-tac-toe", 
         liveLink: "https://tic-tac-toe-kg.netlify.app/"
     },
     {
         id: 2, 
-        image: "/src/assets/images/project-payment-portal.png", 
+        image: "project-payment-portal.png", 
         title: "Payment Website", 
         githubLink:"https://github.com/kaustav-gurey/TheSparksFoundation", 
         liveLink: "https://splendid-rabanadas-10a6f6.netlify.app/"
     },
     {
         id: 3, 
-        image: "/src/assets/images/project-tic-tac-toe.png", 
+        image: "project-tic-tac-toe.png", 
         title: "Tic-tac-toe", 
         githubLink:"https://github.com/kaustav-gurey/tic-tac-toe", 
         liveLink: "https://tic-tac-toe-kg.netlify.app/"
-    },
+    }
 ]
